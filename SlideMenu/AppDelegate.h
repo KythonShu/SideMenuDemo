bf9e7e55f21dd471b46a00a35b9917c7941caa0d
Kython Shu <kythonshu@gmail.com>
2017-03-22 15:00:57 +0800

    Initial Commit

---
+++ SlideMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideMenu
+//
+//  Created by Kython Shu on 2017/3/22.
+//  Copyright © 2017年 Kython Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlideMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideMenu
+//
+//  Created by Kython Shu on 2017/3/22.
+//  Copyright © 2017年 Kython Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
